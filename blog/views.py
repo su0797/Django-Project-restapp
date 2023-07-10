df94b156b868b4820fc0ea07cbab290da23d63be
su12 <ts04109@naver.com>
2023-07-10 16:36:20 +0900

    first commit

---
+++ blog/views.py
@@ -0,0 +1,91 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Post
+from .forms import PostForm
+from .serializers import PostSerializer, CommentSerializer, HashTagSerializer
+
+### Post
+class Index(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serialized_posts = PostSerializer(posts, many=True)# 직렬화
+        return Response(serialized_posts.data)
+
+
+class Write(APIView):
+    # def get(self, request):
+    #     # 사용자 작성 Form 만들어서 보내줌
+    #     pass
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            ## 2차 수정
+            post = serializer.save(writer=request.user)
+            post.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Update(APIView):
+    def get(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    
+    def post(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class Delete(APIView):
+    def post(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        # serializer = PostSerializer(post)
+        # if serializer.is_valid():
+        post.delete()
+        return Response({'message': 'Post deleted'}, status=status.HTTP_204_NO_CONTENT)
+
+
+
+### Comment
+class CommentWrite(APIView):
+    def post(self, request, pk):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            comment = serializer.save(writer=request.user)
+            comment.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDelete(APIView):
+    def post(self, request, pk):
+        comment = Comment.objects.get(pk=pk)
+        comment.delete()
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+
+
+class HashTagWrite(APIView):
+    def post(self, request, pk):
+        serializer = HashTagSerializer(data=request.data)
+        if serializer.is_valid():
+            hashtag = serializer.save(writer=request.user)
+            hashtag.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class HashTagtDelete(APIView):
+    def post(self, request, pk):
+        hashtag = HashTag.objects.get(pk=pk)
+        post_id = hashtag.post_id
+
+        hashtag.delete()
+        serializer = HashTagSerializer(hashtag)
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
---
+++ user/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Profile
+
+
+class ProfileSerializer(serializers.Model):
+    
