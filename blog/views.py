56a89c83b6a4b2f03da1787fccc32af35bc0b42f
su12 <ts04109@naver.com>
2023-07-13 17:51:06 +0900

    models 추가 import

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.throttling import UserRateThrottle
-from .models import Post
+from .models import Post, Comment, HashTag
 from .forms import PostForm
 from .serializers import PostSerializer, CommentSerializer, HashTagSerializer
 
