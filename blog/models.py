67746eadd01ca91f85de80c484a52a3da65d7985
su12 <ts04109@naver.com>
2023-09-13 17:54:07 +0900

    blog model 수정

--- blog/models.py
+++ blog/models.py
@@ -6,16 +6,17 @@ User = get_user_model()
 
 # Create your models here.
 class Post(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=30)
-    content = models.TextField()
+    content = models.TextField(max_length=300, blank=True, null=True)
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Comment(models.Model):
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)
-    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField(max_length=300)
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     
@@ -24,7 +25,7 @@ class Comment(models.Model):
 
 
 class HashTag(models.Model):
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     name = models.CharField(max_length=10)
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     
